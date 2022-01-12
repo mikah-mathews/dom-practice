3163ece4f750fb4e3431266b28bce2b3d64072c7
Mikah Mathews <mikah@outofband.org>
2022-01-11 16:09:49 -0800

    WIP: play around with variables and functions
    Co-Authored-By: Will Watkins <wjwat@onionslice.org>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
-		$("ul#user, ul#webpage").children("li").first().click(function() {
+		$("ul#user").children("li").first().click(function() {
   		$(this).remove();
 		});
 		$("ul#webpage").children("li").first().click(function() {
